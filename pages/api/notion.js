b2f05017eeba8d918da456e0e7e3241e96eb3df4
Mikhail Malyarov <remaik@gmail.com>
2021-07-27 22:44:31 +0300

    refactored /notion

--- pages/api/notion.js
+++ pages/api/notion.js
@@ -5,11 +5,14 @@ import { Client } from '@notionhq/client';
 const notion = new Client({ auth: process.env.NOTION_KEY });
 const databaseId = process.env.NOTION_DATABASE_ID;
 
-export default async function notionApi(req, res) {
-  const response = await notion.databases.query({
-    database_id: databaseId,
-  });
-
-  const data = response.results.map(e => e.properties.Name.title[0]?.plain_text);
-  res.status(200).json(data);
+export default function notionApi(req, res) {
+  notion.databases
+    .query({
+      database_id: databaseId,
+    })
+    .then(response =>
+      response.results.map(e => e.properties.Name.title[0]?.plain_text)
+    )
+    .then(data => res.status(200).json(data))
+    .catch(e => res.status(500).send({message: "Произошла ошибка"}))
 }
